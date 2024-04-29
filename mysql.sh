f3c3cad7d15490735a1f7f90e662730fa49cf8eb
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-29 22:03:28 +0530

    expenses project

--- mysql.sh
+++ mysql.sh
@@ -38,5 +38,11 @@ Validate $? "Enabling mysql server"
 systemctl start mysqld &>>$Logfile
 Validate $? "Starting mysql server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$Logfile
-Validate $? "Setting up the root password for mysql server"
+mysql -h db.sekhardevops.online -uroot -pExpenseApp@1 -e 'SHOW DATABASES;' &>>$Logfile
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$Logfile
+    Validate $? "Setting up the root password for mysql server"
+else
+    echo "Mysql passowrd setup already done ...$Y Skipping..$N"
+fi
