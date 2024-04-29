9f13ce8d4c6df26c8c1795eda5f5152577844d44
somasekhar-devops <somasekhargoudm@gmail.com>
2024-04-29 22:40:59 +0530

    expenses project

--- backend.sh
+++ backend.sh
@@ -9,6 +9,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "Plese enter mysql DB Password : "
+read -s mysql_root_password
+
 USERROLE=$(id -u)
 
 if [ $USERROLE -ne 0 ]
@@ -47,7 +50,41 @@ else
     echo -e "expense user already created... $Y Skipping $N"
 fi
 
+mkdir -p /app &>>$Logfile
+Validate $? "Creating app directory"
 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$Logfile
+Validate $? "Downloading backend code"
+
+cd /app &>>$Logfile
+Validate $? "movng to app directory"
+
+unzip /tmp/backend.zip &>>$Logfile
+Validate $? "Unzipping the backend code"
+
+npm install &>>$Logfile
+Validate $? "Installing nodejs dependencies"
+
+cp /home/ec2-user/expenses-shell/backend.service /etc/systemd/system/backend.service &>>$Logfile
+Validate $? "Copying backend service"
+
+systemctl daemon-reload &>>$Logfile
+Validate $? "Daemon reload"
+
+systemctl start backend &>>$Logfile
+Validate $? "Starting Backend"
+
+systemctl enable backend &>>$Logfile
+Validate $? "Enabling Backend"
+
+dnf install mysql -y &>>$Logfile
+Validate $? "Installing mysql client"
+
+mysql -h db.sekhardevops.online -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$Logfile
+Validate $? "Setting up mysql client root password"
+
+systemctl restart backend &>>$Logfile
+Validate $? "Restarting the backend"
 
 
 
